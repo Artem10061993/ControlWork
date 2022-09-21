3e42277b7e7a5a0bf9e26d514a64ea9411314057
Artem Kandakov <Temofey1993@gmail.com>
2022-09-21 11:05:36 +0500

    Контрольная работа + файл gitignore

---
+++ Exercise/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] array1 = new string[5] {"wefwefw", "wceec", "cefef", "87", "xd"};
+int count = 0;
+for (int i = 0; i < array1.Length; i++)
+{
+if (array1[i].Length <= 3)
+    {
+       count++;
+    }
+    Console.WriteLine();
+}
+ Console.WriteLine("Число элементов массива содержащие в себе 3 и менее символов = "+count);
+ 
+ string[] array2 = new string[count];
+ int j = 0;
+ for (int i = 0; i < array1.Length; i++)
+{
+    if (array1[i].Length <= 3)
+        {
+        array2[j]=array1[i];
+        Console.WriteLine(array2[j]);
+        j++;
+        }
+}
